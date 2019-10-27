8cdde8b75198926e6e72fe8221410259f9207c81
deskninja <30937113+deskninja@users.noreply.github.com>
2019-10-26 22:07:56 -0600

    Skeleton Project
    All necessary classes and text files to complete the project.

---
+++ assignment07/BSTSpellChecker.java
@@ -0,0 +1,12 @@
+package assignment07;
+
+import components.list.List;
+
+public class BSTSpellChecker implements SpellChecker {
+  // TODO create any field or helper methods
+
+  // TODO constructor
+
+  // TODO implement methods from the interface
+
+}
---
+++ assignment07/BinarySearchTreeOfStrings.java
@@ -0,0 +1,87 @@
+package assignment07;
+
+/**
+ * A binary search tree implementation from scratch.
+ * 
+ * @author Swaroop Joshi
+ *
+ */
+public class BinarySearchTreeOfStrings {
+  // TODO declare Node, private fields, etc.
+
+  /**
+   * No argument constructor.
+   */
+  public BinarySearchTreeOfStrings() {
+    // TODO implement this method
+  }
+
+  /**
+   * Removes all the nodes in this tree.
+   * 
+   * @modifies this tree
+   */
+  public void clear() {
+    // TODO implement this method
+  }
+
+  /**
+   * Inserts the element {@code x} at the appropriate position in this tree.
+   * 
+   * @param x element to be inserted
+   * @modifies this tree
+   */
+  public void insert(String x) {
+    // TODO implement this method
+  }
+
+  /**
+   * Removes the element {@code x} from this tree.
+   * 
+   * @param x element to be removed
+   * @modifies this tree
+   * @requires {@code x} is in {@code this}
+   */
+  public void remove(String x) {
+    // TODO implement this method
+  }
+
+  /**
+   * Reports the number of nodes in this tree.
+   * 
+   * @return size of {@code this}
+   */
+  public int size() {
+    return 0; // TODO implement this method
+  }
+
+  /**
+   * Reports whether this tree contains {@code x}.
+   * 
+   * @param x element to search
+   * @return true iff this contains x
+   */
+  public boolean contains(String x) {
+    return false; // TODO implement this method
+  }
+
+  /**
+   * Returns the root of this tree.
+   * 
+   * @return root of this tree
+   * @requires this is not empty
+   */
+  public String root() {
+    return null; // TODO implement this method
+  }
+
+  @Override
+  public String toString() {
+    return null; // TODO implement this method
+  }
+
+  /*
+   * Helper methods
+   */
+  // TODO create private helper methods if required
+}
