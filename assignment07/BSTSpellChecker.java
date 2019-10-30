0159114009e531b50105e3a6bfad1b9f59c8ae6f
JonathanOliveros <35575171+JonathanOliveros@users.noreply.github.com>
2019-10-30 10:27:48 -0600

    added to loadValidWords and misspelledWords
    cannot remember for the life of me how to surround a file with try/catch but other than that I believe it should be working properly

--- assignment07/BSTSpellChecker.java
+++ assignment07/BSTSpellChecker.java
@@ -1,29 +1,65 @@
 package assignment07;
 
-import components.list.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+import components.list.List;
+import components.list.ListOnArrays;
+
+/**
+ * @author Joshua Wells, Jonathan Oliveros
+ *
+ */
 public class BSTSpellChecker implements SpellChecker {
 
+	private BinarySearchTreeOfStrings validWords;
+	private List<String> misspelledWords;
+
 	public BSTSpellChecker() {
-		//TODO: implement
+		clear();
+		validWords = new BinarySearchTreeOfStrings();
 	}
-	
+
 	@Override
 	public void loadValidWords(String filename) {
-		// TODO Auto-generated method stub
-		
+		File file = new File(filename);
+		Scanner s = new Scanner(file);
+		while (s.hasNext()) {
+			validWords.insert(s.next());
+		}
+		s.close();
 	}
-	
+
 	@Override
 	public List<String> misspelledWords(String filename) {
-		// TODO Auto-generated method stub
-		return null;
+		File file = new File(filename);
+		Scanner s = new Scanner(file);
+		String word = "";
+		misspelledWords.add("[");
+		if (!validWords.contains(word)) {
+			misspelledWords.add(word);
+			while (s.hasNext()) {
+				word = s.next();
+				if (!validWords.contains(word)) {
+					misspelledWords.add(word);
+				}
+				misspelledWords.add("]");
+			}
+		}
+		s.close();
+		System.out.println(misspelledWords.toString());
+		return misspelledWords;
 	}
-	
+
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		misspelledWords = new ListOnArrays<String>();
 	}
+	// TODO create any field or helper methods
+
+	// TODO constructor
+
+	// TODO implement methods from the interface
 
 }
