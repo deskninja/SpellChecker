725ea1532c30684961fb97111142c50e301abb60
deskninja <30937113+deskninja@users.noreply.github.com>
2019-10-30 13:22:34 -0600

    Can now read files
    We would just need a new method to read the input text, it can read the valid word files though, and we can use it, then format correctly to get the other files

--- assignment07/BSTSpellChecker.java
+++ assignment07/BSTSpellChecker.java
@@ -1,11 +1,34 @@
 package assignment07;
 
+import java.util.ArrayList;
+
 import components.list.List;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
 
 public class BSTSpellChecker implements SpellChecker {
 
+	private BinarySearchTreeOfStrings dictionary;
+	
+	/**
+	 * Pulls the words from the file and puts them into an arrayList.
+	 * @param filename being read.
+	 * @return list of words.
+	 */
+	private static ArrayList<String> readFile(String filename){
+		SimpleReader file = new SimpleReader1L(filename); 
+		ArrayList<String> list = new ArrayList<>();
+		String line = file.nextLine();
+		while (!file.atEOS()) {  
+			list.add(line);
+			line = file.nextLine();
+		}
+		file.close();
+		return list;
+	}
+	
 	public BSTSpellChecker() {
-		//TODO: implement
+		clear();
 	}
 	
 	@Override
@@ -22,8 +45,7 @@ public class BSTSpellChecker implements SpellChecker {
 	
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		dictionary = new BinarySearchTreeOfStrings();
 	}
 
 }
