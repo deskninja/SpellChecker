e396f84ea398e33bacd277e5463590d6f8c4643e
deskninja <30937113+deskninja@users.noreply.github.com>
2019-10-30 18:58:43 -0600

    timing

--- assignment07/Timing.java
+++ assignment07/Timing.java
@@ -2,10 +2,13 @@ package assignment07;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import components.bintree.BalancedBST1;
 import components.list.List;
+import components.list.ListOnArrays;
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 
@@ -17,20 +20,48 @@ public class Timing {
 		long start = System.nanoTime();
 		sc.loadValidWords("src/assignment07/dictionary.txt");
 		long stop = System.nanoTime();
-		System.out.println(stop - start);
+		System.out.println("BSTInsert " + (stop - start));
 	}
 	
 	@Test
-	public void removeBSTSpellCheckerTime() {
+	public void removeBSTSpellCheckerTimeOrderedInsert() {
 		SpellChecker sc = new BSTSpellChecker();
+		sc.loadValidWords("src/assignment07/dictionary.txt");
 		long start = System.nanoTime();
 		sc.misspelledWords("src/assignment07/bible.txt");
 		long stop = System.nanoTime();
-		System.out.println(stop - start);
+		System.out.println("BSTContains " + (stop - start));
+	}
+	
+	@Test
+	public void removeBSTSpellCheckerTimeRandomInsert() {
+		SpellChecker sc = new BSTSpellChecker();
+		sc.loadValidWords("src/assignment07/dictionary.txt");
+		long start = System.nanoTime();
+		sc.misspelledWords("src/assignment07/bible.txt");
+		long stop = System.nanoTime();
+		System.out.println("BSTContainsRandomInsert " + (stop - start));
 	}
 
 	@Test
 	public void insertOtherSpellChecker() {
+		BalancedBST1<String> bc = new BalancedBST1<>();
+		SimpleReader s = new SimpleReader1L("src/assignment07/dictionary.txt");
+		//insert the valid words to bc
+		long start = System.nanoTime();
+		while (!s.atEOS()) {
+			String line = s.nextLine();
+			for (String str : line.split(" ")) {
+				bc.insert(str);
+			}
+		}	
+		long stop = System.nanoTime();
+		System.out.println("OtherInsert " + (stop - start));
+		s.close();
+	}
+	
+	@Test
+	public void containsOtherSpellChecker() {
 		BalancedBST1<String> bc = new BalancedBST1<>();
 		SimpleReader s = new SimpleReader1L("src/assignment07/dictionary.txt");
 		//insert the valid words to bc
@@ -41,5 +72,42 @@ public class Timing {
 			}
 		}	
 		s.close();
+		
+		long start = System.nanoTime();
+		List<String> misspelledWords = new ListOnArrays<String>();
+		SimpleReader bible = new SimpleReader1L("src/assignment07/bible.txt");
+		while (!bible.atEOS()) {
+			String line = bible.nextLine();
+			for (String str : line.split(" ")) {
+				if (!bc.contains(str)) {
+					misspelledWords.add(str);
+				}
+			}
+		}
+		long stop = System.nanoTime();
+		System.out.println("OtherContains " + (stop - start));
+		bible.close();
+	}
+	
+	@Test
+	public void randomOrder() {
+		SimpleReader s = new SimpleReader1L("src/assignment07/dictionary.txt");
+		List<String> validWords = new ListOnArrays<String>();
+		while (!s.atEOS()) {
+			String line = s.nextLine();
+			for (String str : line.split(" ")) {
+				validWords.add(str);
+			}
+		}
+		s.close();
+		List<String> randomWords = new ListOnArrays<String>();
+		Random r = new Random();
+		while(validWords.size() > 0) {
+			String nextItem = validWords.remove(r.nextInt(validWords.size()));
+			randomWords.add(nextItem);
+		}
+		for(String line: randomWords) {
+			System.out.println(line);
+		}
 	}
 }
